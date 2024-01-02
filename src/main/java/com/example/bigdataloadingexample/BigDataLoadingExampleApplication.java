b66b6942022a91d55adede4d69f43e095b80221d
Adam Szałański <adamus8895@gmail.com>
2024-01-02 13:04:50 +0100

    initial commit

---
+++ src/main/java/com/example/bigdataloadingexample/BigDataLoadingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigdataloadingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataLoadingExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigDataLoadingExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bigdataloadingexample/BigDataLoadingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigdataloadingexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDataLoadingExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
