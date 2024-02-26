02284e92b637ed961e95c4a6c38afbcf1f1078df
Adam Szałański <adsn@softwaremind.com>
2024-02-26 12:22:37 +0100

    refactor: db-initialization

---
+++ db-config/init-db.sql
@@ -0,0 +1,14 @@
+CREATE SEQUENCE IF NOT EXISTS product_sequence START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE products
+(
+    id            BIGINT NOT NULL,
+    name          VARCHAR(255),
+    category      VARCHAR(255),
+    creation_date date,
+    author        VARCHAR(255),
+    release_date  date,
+    publisher     VARCHAR(255),
+    review_score  DOUBLE PRECISION,
+    CONSTRAINT pk_products PRIMARY KEY (id)
+);
